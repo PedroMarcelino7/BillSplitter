20ecd25d8754d3346ee01e759a297a12c8674e19
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-08 23:06:19 -0300

    bill, tip percentage, tip and total updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,84 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [bill, setBill] = useState(0)
+  const [tipPercentage, setTipPercentage] = useState(0)
+  const [tip, setTip] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [split, setSplit] = useState(0)
+  const [billEach, setBillEach] = useState(0)
+
+  const handleBillChange = (e) => {
+    setBill(parseInt(e.target.value))
+  }
+
+  const handleTipChange = (e) => {
+    const value = parseInt(e.target.value)
+
+    setTipPercentage(value)
+
+    const tipCalc = ((value / 100) * bill)
+
+    setTip(parseFloat(tipCalc.toFixed(2)))
+  }
+
+  useEffect(() => {
+    setTotal(bill + tip)
+  }, [bill, tipPercentage])
+
   return (
     <>
-      <main oninput="update()">
+      <main>
         <h1>Bill Splitter</h1>
         <section>
-          <div class="bill">
-            <label for="yourBill">Bill</label>
-            <input type="number" placeholder="Your bill" id="yourBill" />
-          </div>
-          <div>
-            <div class="space-between">
-              <label for="">Select tip</label><span id="tipPercent"></span>
+          <form className="bill">
+            <label htmlFor="yourBill">Bill</label>
+            <input
+              onChange={handleBillChange}
+              type="number"
+              placeholder="Your bill"
+            />
+          </form>
+
+          <form>
+            <div className="space-between">
+              <label>Select tip</label><span>{tipPercentage} %</span>
             </div>
-            <input type="range" value="0" id="tipInput" class="range" />
-          </div>
-          <div class="space-between">
+            <input
+              onChange={handleTipChange}
+              type="range"
+              value={tipPercentage}
+              className="range"
+            />
+          </form>
+
+          <div className="space-between">
             <span>Tip</span>
-            <span id="tipValue"></span>
+            <span>$ {tip}</span>
           </div>
+
           <hr />
-          <div class="space-between total">
+
+          <div className="space-between total">
             <span>Total</span>
-            <span id="totalWithTip"></span>
+            <span>$ {total}</span>
           </div>
         </section>
 
         <section>
           <div>
-            <div class="space-between">
-              <label for="splitInput">Split</label>
-              <span id="splitValue"></span>
+            <div className="space-between">
+              <label htmlFor="splitInput">Split</label>
+              <span></span>
             </div>
-            <input type="range" min="1" max="10" value="1" id="splitInput" class="range" />
+            <input type="range" min="1" max="10" value="1" className="range" />
           </div>
-          <div class="space-between">
+
+          <div className="space-between">
             <span>Bill each</span>
-            <span id="billEach"></span>
+            <span></span>
           </div>
         </section>
       </main>
